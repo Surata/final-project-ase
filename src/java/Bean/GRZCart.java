d5927a20778304d2b648d418d1704e3672147b97
Edi Surata <e.surata@gmail.com>
2014-05-19 20:49:14 +0700

    Add Cart class

---
+++ src/java/Bean/GRZCart.java
@@ -0,0 +1,54 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Bean;
+
+/**
+ *
+ * @author edista
+ */
+public class GRZCart {
+    private int cartID;
+    private int userID;
+    private float total;
+    private int status;
+    private String date;
+
+    public GRZCart() {
+    }
+
+    public int getCartID() {
+        return cartID;
+    }
+
+    public void setCartID(int cartID) {
+        this.cartID = cartID;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public float getTotal() {
+        return total;
+    }
+
+    public void setTotal(float total) {
+        this.total = total;
+    }
+    
+    
+}
